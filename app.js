47d6c952141d3cd4d41d1873a482b49b371e8279
Danielle H <daheber@gmail.com>
2016-04-02 12:38:33 -0700

    base functions added

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+var taskInput = document.getElementById("newTask");
+var incompleteTasksHolder = document.getElementById("incompleteTasks");
+var completeTasksHolder = document.getElementById("completeTasks");
 
+//add task
+var addTask = function(){
+  console.log('adding task...');
+};
+
+//edit task
+var editTask = function(){
+  console.log('editing task...');
+};
+
+//delete task
+var deleteTask = function(){
+  console.log('deleting task...');
+};
+
+//mark task complete
+var completeTask = function(){
+  console.log('task marked complete...');
+};
+
+//mark task incomplete
+var completeTask = function(){
+  console.log('task marked incomplete...');
+};
+
+//event listeners
